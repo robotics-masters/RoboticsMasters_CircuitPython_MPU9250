c5119528dacc32ab8a6c5ab9578596c7da188df2
wallarug <wallarug@coliemore.com.au>
2019-06-03 14:05:12 +0100

    temperature seems more stable than before.  Reporting about 20C most of the time which is about right.  Updated address to 0x69

--- examples/imu.py
+++ examples/imu.py
@@ -5,7 +5,7 @@ from time import sleep
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
-sensor = adafruit_mpu9250.MPU9250_I2C(i2c, 0x68)
+sensor = adafruit_mpu9250.MPU9250_I2C(i2c, 0x69)
 
 while True:
     #print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(*sensor.acceleration))
